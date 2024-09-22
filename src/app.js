ab04eeaa29fb69cba82480ca2e153dca33d89472
Imran Ali <imranali.code@gmail.com>
2024-09-23 01:08:50 +0530

    created a server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use("/test", (req, res) => {
+  res.send("Hello namaste from the server");
+});
+app.use("/hello", (req, res) => {
+  res.send("Hello Hello Hello Hello......");
+});
+app.use("/", (req, res) => {
+  res.send("Hello from the dashboard");
+});
+app.listen(7777, () => {
+  console.log("server is ssucessfully listening on port 7777..");
+});
