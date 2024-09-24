884b732a19189d7bf50922205b7d6ff38912e441
Imran Ali <imranali.code@gmail.com>
2024-09-24 16:16:21 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Imran",
+    lastname: "Ali",
+  });
+});
 app.use("/test", (req, res) => {
   res.send("Hello namaste from the server");
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Hello Hello......");
-});
-app.use("/", (req, res) => {
-  res.send("Hello from the dashboard");
-});
+// app.use("/hello", (req, res) => {
+//   res.send("Hello Hello Hello Hello......");
+// });
+//  app.use("/", (req, res) => {
+//   res.send("Hello from the dashboard");
+// });
 app.listen(7777, () => {
   console.log("server is ssucessfully listening on port 7777..");
 });
