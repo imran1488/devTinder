759d41f999b76369def0053c3eb41cef075562eb
Imran Ali <imranali.code@gmail.com>
2024-09-26 03:20:10 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
-const { adminAuth, userAuth } = require("./middlewares/auth");
-app.use("/admin", adminAuth);
+const User = require("./models/user");
 
-app.post("/user/login", (req, res) => {
-  res.send("User Logged in Successfully");
-});
-app.use("/user", userAuth, (req, res) => {
-  res.send("User Data Sent");
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Imran",
+    lastName: "Ali",
+    emailId: "imranali.code@gmail.com",
+    password: "code0090",
+  };
+  const user = new User(userObj);
+  try {
+    await user.save();
+    res.send("User added sucessfully!");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
 });
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data sent");
-});
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted all User");
-});
-app.listen(3001, () => {
-  console.log("server is ssucessfully listening on port 3001..");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3001, () => {
+      console.log("Server is successfully listening on port 3001");
+    });
+  })
+  .catch(() => {
+    console.error("Database connection cannot be established");
+  });
+//  mongodb+srv://imran_1:<db_password>@cluster0.wvgimil.mongodb.net/
+//  mongodb+srv://imran_1:<db_password>@cluster0.wvgimil.mongodb.net/
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+// mongodb cluster0 user => imran_1:O3h1ZA9CpMqorFlD
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://imran_1:O3h1ZA9CpMqorFlD@cluster0.wvgimil.mongodb.net/devTinder"
+  );
+};
+
+//const connectDB = connect();
+module.exports = connectDB;
