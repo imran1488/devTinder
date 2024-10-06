ecc07164dcbbfef5e2d01d42a90904cd9ceaf1e8
Imran Ali <imranali.code@gmail.com>
2024-10-06 14:27:51 +0530

    logout + editprofile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
-
-profileRouter.get("/profile", userAuth, async (req, res) => {
+const { validateEditProfileData } = require("../utils/validation");
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
   } catch (err) {
     res.status(400).send("ERROR: " + err);
+  }
+});
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+    const loggedInuser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInuser[key] = req.body[key]));
+    await loggedInuser.save();
+    res.json({
+      message: `${loggedInuser.firstName} Profile Updated Successfully`,
+      data: loggedInuser,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 });
 
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -9,7 +9,24 @@ const validateSignUpData = (req) => {
     throw new Error("plese eneter a strong password");
   }
 };
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "skills",
+    "about",
+  ];
 
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+  return isEditAllowed;
+};
 module.exports = {
   validateSignUpData,
+  validateEditProfileData,
 };
