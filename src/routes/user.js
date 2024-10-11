c57f7b48a13e9a48c01ad1273f3e8d8d7d78aadc
Imran Ali <imranali.code@gmail.com>
2024-10-11 19:53:29 +0530

    building API & pagination improved response format in user

--- src/routes/user.js
+++ src/routes/user.js
@@ -77,7 +77,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
       .select(USER_SAFE_DATA)
       .skip(skip)
       .limit(limit);
-    res.send(users);
+    res.send({ data: users });
     //res.send(connectionRequests);
   } catch (err) {
     res.status(400).json({ message: err.message });
