c4e1eb13b0761fe1083a710a39fc972c6506a954
Imran Ali <imranali.code@gmail.com>
2024-10-11 14:17:37 +0530

    ref, Populate & thought process of writing APIs

--- src/app.js
+++ src/app.js
@@ -10,10 +10,12 @@ app.use(cookieParser());
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
+const userRequests = require("./routes/user");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
+app.use("/", userRequests);
 connectDB()
   .then(() => {
     console.log("Database connection established...");
--- src/routes/request.js
+++ src/routes/request.js
@@ -48,4 +48,35 @@ requestRouter.post(
     }
   }
 );
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user; //coming form userAuth
+      const { status, requestId } = req.params;
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: "Status not allowed!" });
+      }
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connection Request not found" });
+      }
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+      res.status(200).json({ message: "Connection Request " + status, data });
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);
+    }
+  }
+);
 module.exports = requestRouter;
+
+//ms dhoni sent to rohan
