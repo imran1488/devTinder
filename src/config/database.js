d2dbd9c7e8da5002ac14077c5f81b0bc3c0aca2a
Imran Ali <imranali.code@gmail.com>
2024-10-15 00:40:21 +0530

    commiting to handle the routing through correct response

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://imran_1:znSr1aWn4qKmg6ir@cluster0.wvgimil.mongodb.net/devTinder"
+    "mongodb+srv://imran_1:XfFwbBDjm8Dy5Gz4@cluster0.wvgimil.mongodb.net/devTinder"
   );
 };
 
 //const connectDB = connect();
 module.exports = connectDB;
-// znSr1aWn4qKmg6ir ----> Password
-//mongodb+srv://imran_1:znSr1aWn4qKmg6ir@cluster0.wvgimil.mongodb.net/devTinder
+// XfFwbBDjm8Dy5Gz4 ----> Password
+//mongodb+srv://imran_1:XfFwbBDjm8Dy5Gz4@cluster0.wvgimil.mongodb.net/devTinder
